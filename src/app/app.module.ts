6b1d34713e2a0db15d33e4bb97905ce480a10a86
Meistt <aguslive7@gmail.com>
2023-05-11 17:54:16 -0300

    corrijo componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ChomeComponent } from './components/chome/chome.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductComponent } from './components/product/product.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ChomeComponent,
+    HomeComponent,
+    LoginComponent,
+    ProductComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
